0b75000710fcfc88b956183b4662a310c72eebd0
Fayyaz Ahmad <91311395+Fayyaz-fx@users.noreply.github.com>
2022-01-01 14:15:22 +0000

    first commit
    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [isLit, setIsLit] = useState(false)
+  const [temp, setTemp] = useState(72)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`room ${isLit ? 'lit' : 'dark'}`}>
+      <h1>The room is {isLit ? 'Lit' : 'dark'}</h1>
+      <br />
+
+      <button className='btn' onClick={() => setIsLit(true)}>
+        Switch On
+      </button>
+
+      <button className='btn' onClick={() => setIsLit(false)}>
+        Switch Off
+      </button>
+      <h1>and The temperature is {temp}</h1>
+      <br />
+      <button className='btn' onClick={() => setTemp(temp + 1)}>
+        Increase
+      </button>
+
+      <button className='btn' onClick={() => setTemp(temp - 1)}>
+        Decrease
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
